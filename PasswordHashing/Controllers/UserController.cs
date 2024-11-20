a6be41aed6e7bf2e1ea766492aef66f7cb0f2736
assistant1277 <shivarajghangale2002@gmail.com>
2024-11-20 20:24:24 +0530

    updated

--- PasswordHashing/Controllers/UserController.cs
+++ PasswordHashing/Controllers/UserController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using PasswordHashing.Dtos;
+using PasswordHashing.Models;
 using PasswordHashing.Services;
 using System.ComponentModel.DataAnnotations;
 
@@ -10,6 +12,7 @@ namespace PasswordHashing.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        /*
         private readonly IUserService _userService;
 
         public UserController(IUserService userService)
@@ -41,6 +44,49 @@ namespace PasswordHashing.Controllers
                     .Select(e => e.ErrorMessage));
                 throw new ValidationException($"{errors}");
             }
+            _userService.Login(loginDto);
+            return Ok("Login successful");
+        }
+        */
+
+        private readonly IUserService _userService;
+        private readonly IMapper _mapper;
+
+        public UserController(IUserService userService, IMapper mapper)
+        {
+            _userService = userService;
+            _mapper = mapper;
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register(UserDto userDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                var errors = string.Join(";", ModelState.Values
+                    .SelectMany(v => v.Errors)
+                    .Select(e => e.ErrorMessage));
+                throw new ValidationException($"{errors}");
+            }
+
+            // Use AutoMapper to map UserDto to User if necessary
+            var user = _mapper.Map<User>(userDto);
+
+            _userService.Register(userDto);
+            return Ok("User has registered successfully");
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login(LoginDto loginDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                var errors = string.Join(";", ModelState.Values
+                    .SelectMany(v => v.Errors)
+                    .Select(e => e.ErrorMessage));
+                throw new ValidationException($"{errors}");
+            }
+
             _userService.Login(loginDto);
             return Ok("Login successful");
         }
